4b0bd7dc655362b971ca529ca39ee300ef1e70b7
Nithin KV <gowdanithin232@gmail.com>
2026-06-25 17:56:27 +0530

    Add Task1.java with nested class and main methods

---
+++ DAY-1/Task1.java
@@ -0,0 +1,11 @@
+public class Task1
+{
+    public static void main(String[] args) {
+        System.out.println("D");
+    }
+    class E{
+    public static void main(String[] args) {
+        System.out.println("E");
+    }
+}
+}
