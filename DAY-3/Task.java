cf348fe03847b58c24f0dbfbc2f4ec5a83fc35ca
Nithin KV <gowdanithin232@gmail.com>
2026-06-27 12:17:54 +0530

    Create Task.java

---
+++ DAY-3/Task.java
@@ -0,0 +1,202 @@
+//Some Complex loop problems
+public class Task {
+    public static void main(String[] args) {
+        //primenumber();
+        //primenumberBtwRange();
+        //highestPrimeNumber();
+        //firstNprime();
+        //Automorphic();
+        //amstrongNumber();
+        //reverse();
+        //palindrome();
+        //LCM();
+        //HCF();
+        HCF2();
+    }
+    static void primenumber(){
+        int n=2;
+        boolean isPrime=true;
+        for(int i=2;i<=n/2;i++){
+            if(n%i==0){
+                isPrime=false;
+                break;
+            }
+        }
+        if(isPrime && n>=2){
+            System.out.println("Its a prime Number");
+        }else{
+            System.out.println("Its not a prime number");
+        }
+    }
+    static void primenumberBtwRange(){
+        int a=20;
+        int b=100;
+        for(int n=a;n<=b;n++){
+            boolean flag=true;
+            for(int i=2;i<=n/2;i++){
+                if(n%i==0){
+                    flag=false;
+                    break;
+                }
+            }
+            if(flag){
+                System.out.println(n);
+            }
+        }
+    }
+    static void highestPrimeNumber(){
+        for(int a=100;a>=20;a--){
+            boolean flag=true;
+            for(int i=2;i<=a/2;i++){
+                if(a%i==0){
+                    flag=false;
+                    break;
+                }
+            }
+            if(flag){
+                System.out.println(a);
+                break;
+            }
+        }
+    }
+    static void firstNprime(){
+        int n=5;
+        int count=0;
+        int i=2;
+        while(true){
+            boolean flag=true;
+            for(int j=2;j<=i/2;j++){
+                if(i%j==0){
+                    flag=false;
+                }
+            }
+            if(flag){
+                System.out.println(i);
+                count++;
+                if(count==n){
+                    break;
+                }
+            }
+            i++;
+        }
+    }
+    static void Automorphic(){
+        int n=10;
+        int sq=n*n;
+        boolean flag=true;
+        while(n>0){
+            if((n%10)!=(sq%10)){
+                flag=false;
+                break;
+            }
+            n=n/10;
+            sq=sq/10;
+        }
+        if(flag){
+            System.out.println("Automorphic");
+        }else{
+            System.out.println("Not automorphic");
+        }
+    }
+    static void amstrongNumber(){
+        int n=153;
+        int temp=n;
+        int count=0;
+        while(n>0){
+            count++;
+            n=n/10;
+        }
+        n=temp;
+        int sum=0;
+        while(n>0){
+            int d=n%10;
+            int prod=1;
+            for(int i=1;i<=count;i++){
+                prod=prod*d;
+            }
+            sum=sum+prod;
+            n=n/10;
+        }
+        if(sum==temp){
+            System.out.println("Amstrong Number");
+        }else{
+            System.out.println("Not a Amstrong Number");
+        }
+    }
+    static void reverse(){
+        int n=123;
+        int rev=0;
+        while(n>0){
+            int d=n%10;
+            rev=(rev*10)+d;
+            n=n/10;
+        }
+        System.out.println(rev);
+    }
+    static void palindrome(){
+        int n=121;
+        int rev=0;
+        int temp=n;
+        while(n>0){
+            int d=n%10;
+            rev=(rev*10)+d;
+            n=n/10;
+        }
+        if(temp==rev){
+            System.out.println("Paindrome NUmber");
+        }else{
+            System.out.println("Not a Plaindrome");
+        }
+    }
+    static void LCM(){
+        int a=4;
+        int b=6;
+        int i=a>b?a:b;
+        int n=i;
+        while(true){
+            if((i%a==0)&&(i%b==0)){
+                break;
+            }
+            i=i+n;
+        }
+        System.out.println(i);
+    }
+    static void HCF(){
+        int a=9;
+        int b=27;
+        int i=a<b?a:b;
+        while(true){
+            if((a%i==0)&&(b%i==0)){
+                break;
+            }
+            i--;
+        }
+        System.out.println(i);
+    }
+    static void HCF2(){
+        int a=12;
+        int b=18;
+        int hcf=1;
+        for(int i=1;((i<=a)&&(i<=b));i++){
+            if((a%i==0)&&(b%i==0)){
+                hcf=i;
+                //printing hcf's until getting largest
+                System.out.println(i);
+            }
+        }
+        //last highest hcf
+        System.out.println("s0 the hcf is "+hcf);
+    }
+    static void decimalTObinary(){
+        int n=75;
+        int i=1;
+        int bin=0;
+        while(n>0){
+            int bit=n%2;
+            bin=(bit*i)+bin;
+            n=n/2;
+            i=i*10;
+        }
+        System.out.println(bin);
+    }
+}
